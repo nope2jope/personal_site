41310f40805577b2be39131e11a8f5d1e49a8108
Joseph Kisbye <josephkisbye@gmail.com>
2025-07-02 11:51:18 -0400

    minor improvement

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 const Footer = () => {
+
+    const d = new Date().getFullYear();
+    
     return <div>
         <div className="row buffer"><p></p></div>
         <footer>
 
             <div className="center">
-                <p id="foot-text" className="footer-text">this website was created by me, <a id="foot-name-link" target="_blank" href="https://josephkisbye.com/">Joseph Kisbye</a>. Copyright 2025</p>
+                <p id="foot-text" className="footer-text">this website was created by me, <a id="foot-name-link" target="_blank" href="https://josephkisbye.com/">Joseph Kisbye</a>. Copyright {d}</p>
 
             </div>
         </footer>

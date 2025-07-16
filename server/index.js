a9befc5ead89c4b42a967ce60acc9a344a5c054e
Joseph Kisbye <josephkisbye@gmail.com>
2025-07-16 13:29:11 -0400

    server update

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
